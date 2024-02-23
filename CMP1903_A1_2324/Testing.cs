db4965888e71c9b885258608cd3d1ba860426e16
Dylan0411 <dpwhitney535@gmail.com>
2024-02-23 17:58:02 +0000

    main

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -17,11 +17,15 @@ namespace CMP1903_A1_2324
         {
             testGame.Run();  //Calls the 'Run' method in the 'testGame' object.
 
-            Debug.Assert(testGame.D1Value > 0 && testGame.D1Value < 7, "Die1 didnt roll a number between 1 and 6"); //checking that the "D1Value" variable (which represents the first dice roll) is between 1 and 6
-            Debug.Assert(testGame.D2Value > 0 && testGame.D2Value < 7, "Die2 didnt roll a number between 1 and 6"); //checking that the "D2Value" variable (which represents the second dice roll) is between 1 and 6
-            Debug.Assert(testGame.D3Value > 0 && testGame.D3Value < 7, "Die3 didnt roll a number between 1 and 6"); //checking that the "D3Value" variable (which represents the third dice roll) is between 1 and 6
+            //checking that the "D1Value" variable (which represents the first dice roll) is between 1 and 6
+            Debug.Assert(testGame.D1Value > 0 && testGame.D1Value < 7, "Die1 didnt roll a number between 1 and 6");
+            //checking that the "D2Value" variable (which represents the second dice roll) is between 1 and 6
+            Debug.Assert(testGame.D2Value > 0 && testGame.D2Value < 7, "Die2 didnt roll a number between 1 and 6");
+            //checking that the "D3Value" variable (which represents the third dice roll) is between 1 and 6
+            Debug.Assert(testGame.D3Value > 0 && testGame.D3Value < 7, "Die3 didnt roll a number between 1 and 6");
 
-            Debug.Assert(testGame.sum == testGame.D1Value + testGame.D2Value + testGame.D3Value, "ERROR: SUM INCORRECT"); //checking that the 'sum' variable actually equals the result of adding together each dice rolls value.
+            //checking that the 'sum' variable actually equals the result of adding together each dice rolls value
+            Debug.Assert(testGame.sum == testGame.D1Value + testGame.D2Value + testGame.D3Value, "ERROR: SUM INCORRECT");
         }
     }
 }
