af416bea6000d62e3dc1cf3d2b9b464a2291ad62
Dylan0411 <dpwhitney535@gmail.com>
2024-04-30 23:41:06 +0100

    v10.3: - improved comments within the die class

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
+    /// <summary>
+    /// this class is used to create dice gameobjects that roll numbers between 1 and 6
+    /// </summary>
     internal class Die
     {
-        //-------------------------------Variables-------------------------------
         private int currentDieValue; //This variable will hold the value of the rolled dice.
-
-        //-------------------------------Constructors-------------------------------
         private static Random value = new Random(); //Creates a new 'random' object called 'value'.
 
-        //-------------------------------Methods-------------------------------
+        /// <summary>
+        /// picks a random value between 1 and 6 and returns it within the "currentDieValue" variable.
+        /// </summary>
         public int Roll() //When this is called the dice is rolled (a number between 1-6 is chosen).
         {
             currentDieValue = value.Next(1, 7); //Generate a number between 1 and 6.
